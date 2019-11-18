ec87e40ea902d08bce250afe3bd5db53aa1d1688
diegobodner <38262993+diegobodner@users.noreply.github.com>
2019-11-18 20:19:08 -0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -10,9 +10,13 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 #export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/gdr8uz7fzhxff80/export.pkl?dl=1'
 
-export_file_name = 'export.pkl'
+#export_file_url = 'https://www.dropbox.com/s/gdr8uz7fzhxff80/export.pkl?dl=1'
+export_file_url = 'https://www.dropbox.com/s/i7cyr2ia5xn9lj5/stage-1.pth?dl=1'
+
+
+#export_file_name = 'export.pkl'
+export_file_name = 'stage-1.pth'
 
 #classes = ['black', 'grizzly', 'teddys']
 
